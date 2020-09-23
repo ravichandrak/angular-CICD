f1543e1c356968a4bb368ba0a3f19ee2452372c3
unknown <ravichandrak89@gmail.com>
2020-09-23 09:54:08 +0530

    changed app title

--- src/app/books-web-header-component/books-web-header.component.ts
+++ src/app/books-web-header-component/books-web-header.component.ts
@@ -1,28 +1,21 @@
 import {Component, Input} from '@angular/core';
 
 @Component({
-    selector: 'app-booksweb-header',  //custom html element or component identifier
+    selector: 'app-booksweb-header',
     styleUrls:[
         'books-web-header.component.css'
     ],
     templateUrl:'books-web-header.component.html'    
 })
 export class BooksWebHeaderComponent {
-    //this is a javascript code
-    //the data can come from anywhere like
-    //database, ajax, calculated etc
-    //so it is a dynamic data
-    @Input() public title = 'Web App';
     
-    public subTitle = 'Where book lovers come...';
-    public time = '';
+    @Input() public title = 'Web App - Production';
+    
     public selectedIndex;
 
     constructor() {
-        this.selectedIndex =0;
-        setInterval(() => {
-            this.time = new Date().toLocaleTimeString()
-        }, 1000); //keep updating every 1 sec
+        this.selectedIndex = 0;
+        
     }
     addActiveStyle(menuIndex) {
         this.selectedIndex = menuIndex;
